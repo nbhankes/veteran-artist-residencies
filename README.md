b3dc36caf7c3e90681abffecf3eb8ae55f37c7cd
nate-hankes-hydrofarm <nate.hankes@hydrofarm.com>
2022-05-27 15:01:36 -0700

    Form updates to netlify

---
+++ README.md
@@ -0,0 +1,3 @@
+#### Veteran Artist Residencies is a non-profit providing paid artistic residencies for post-9/11 veterans pursuing careers in writing.
+
+The live website can be viewed at www.veteranarts.org
---
+++ default.js
@@ -0,0 +1,62 @@
+//! Burger Menu
+
+function openMobileNav() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+
+//! Donation Modal
+
+const modal = document.querySelector(".modal");
+const activate = document.querySelector(".activate");
+const closeButton = document.querySelector(".ink-people-redirect-button");
+
+/*toggle on button click*/
+function toggleModal() {
+    modal.classList.toggle("active");
+}
+
+function windowOnClick(event) {
+    if (event.target === modal) {
+        toggleModal();
+    }
+}
+
+/*event listeners*/
+activate.addEventListener("click", toggleModal);
+closeButton.addEventListener("click", toggleModal);
+window.addEventListener("click", windowOnClick);
+
+
+//!Application Form Validation
+
+const form = document.querySelector("#application-form");
+
+let phoneInput = document.querySelector("#form-phone");
+phoneInput.addEventListener("input", validatePhone);
+
+//This regex ids phone numbers: https://www.oreilly.com/library/view/regular-expressions-cookbook/9780596802837/ch04s02.html
+const regFormatPhone = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
+const regFormatPhoneFirst = /^\(?([0-9]{3})\)?$/
+const regFormatPhoneSecond = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})$/
+
+function validatePhone(e) {
+  let target = e.target;
+
+  if (target.name == "phone") {
+    //validate phone input
+    if(regFormatPhoneFirst.test(target.value) && target.value.length === 3) {
+      let formattedPhone = target.value.replace(regFormatPhoneFirst, "($&) ")
+      phoneInput.value = formattedPhone
+    }
+
+    if (regFormatPhoneSecond.test(target.value)) {
+    let formattedPhoneTwo = target.value.replace(regFormatPhoneSecond, "($1) $2-")
+    phoneInput.value = formattedPhoneTwo
+    }
+  }
+}
